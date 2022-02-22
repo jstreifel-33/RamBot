dbd29152e421707d935b5febadd611ba2c9e393f
Joe Streifel <josephstreifel@gmail.com>
2022-02-21 17:03:45 -0800

    Adds print of all guild members

--- ram_bot/bot.py
+++ ram_bot/bot.py
@@ -3,13 +3,27 @@ import os
 import discord
 from dotenv import load_dotenv
 
+intents = discord.Intents().all()
+# client = discord.Bot(prefix = '', intents=intents)
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+        )
+        
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
